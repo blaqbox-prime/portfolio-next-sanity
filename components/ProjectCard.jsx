1bf8dbe3529cd64b8bab61bdbcfabda44ca3db96
Karabo Sambo <karabosambo@outlook.com>
2022-10-18 20:50:14 +0200

    Fixed github links

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,42 +1,60 @@
-import { Box, Flex, Icon, Link, Text } from "@chakra-ui/react"
-import {GoMarkGithub, GoGlobe} from 'react-icons/go'
+import { Box, Flex, Icon, Link, Text, useColorModeValue } from "@chakra-ui/react";
+import { GoMarkGithub, GoGlobe } from "react-icons/go";
 
+function ProjectCard({ project }) {
 
-function ProjectCard({project}) {
-  <Box as={'article'} boxShadow={'lg'} borderRadius={'none'} my={'5'} p={5}> 
-  <Text fontSize={'x-small'} textTransform={'uppercase'} fontWeight={'bold'} color={'blackAlpha.400'} mb={'4'}>{project.tags.join(' | ')}</Text>
-  <Flex alignItems={'baseline'} mb={'3'}>
-    <Link href={project.url} isExternal>
-    <Text as={'h1'}
-    fontWeight={'bold'}
-    textAlign={'left'}
-    fontSize={['lg' , 'xl']}
-    >{project.title} | {project.subtitle}
+  const fadedText = useColorModeValue('blackAlpha.600', 'whiteAlpha.600')
+  const fadedLine = useColorModeValue('blackAlpha.800', 'whiteAlpha.800')
+
+  return (
+    <Box as={"article"} boxShadow={"lg"} borderRadius={"none"} my={"5"} p={5}>
+    <Text
+      fontSize={"x-small"}
+      textTransform={"uppercase"}
+      fontWeight={"bold"}
+      color={fadedText}
+      mb={"4"}
+    >
+      {project.tags.join(" | ")}
     </Text>
-    </Link>
-  </Flex>
+    <Flex alignItems={"baseline"} mb={"3"}>
+      <Link href={project.url} isExternal>
+        <Text
+          as={"h1"}
+          fontWeight={"bold"}
+          textAlign={"left"}
+          fontSize={["lg", "xl"]}
+        >
+          {project.title} | {project.subtitle}
+        </Text>
+      </Link>
+    </Flex>
 
-<Text
-textAlign={'left'}
-fontSize={['sm','md']}
-fontWeight={'normal'}
-maxWidth={'90%'}
->
-{project.description}
-</Text>
-<Link href={project.source_code} isExternal
-outlineColor={'transparent'}
-_hover={{
-color: 'blue.600',
-transition: 'color .25s'
-}}
->
-<Flex mt={'4'} w='full' alignItems="center">
-<Icon fontSize={'md'} as={GoMarkGithub} mr='3' />
-<Text fontSize={'smaller'}>Github Repository</Text>
-</Flex>
-</Link>
-</Box>
+    <Text
+      textAlign={"left"}
+      fontSize={["sm", "md"]}
+      fontWeight={"normal"}
+      maxWidth={"90%"}
+    >
+      {project.description}
+    </Text>
+    <Link
+      href={project.source}
+      isExternal
+      outlineColor={"transparent"}
+      _hover={{
+        color: "blue.600",
+        transition: "color .25s",
+      }}
+    >
+      <Flex mt={"4"} w="full" alignItems="center">
+        <Icon fontSize={"md"} as={GoMarkGithub} mr="3" />
+        <Text fontSize={"smaller"}>Github Repository</Text>
+      </Flex>
+    </Link>
+  </Box>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
+
--- pages/index.js
+++ pages/index.js
@@ -102,40 +102,7 @@ export default function Home({projects}) {
           <Box as={'section'} id="projects">
           {
               projects && projects.map((project) => (
-              <Box key={project._id} as={'article'} boxShadow={'lg'} borderRadius={'none'} my={'5'} p={5}> 
-                <Text fontSize={'x-small'} textTransform={'uppercase'} fontWeight={'bold'} color={fadedText} mb={'4'}>{project.tags.join(' | ')}</Text>
-                <Flex alignItems={'baseline'} mb={'3'}>
-                  <Link href={project.url} isExternal>
-                  <Text as={'h1'}
-                  fontWeight={'bold'}
-                  textAlign={'left'}
-                  fontSize={['lg' , 'xl']}
-                  >{project.title} | {project.subtitle}
-                  </Text>
-                  </Link>
-                </Flex>
-
-      <Text
-      textAlign={'left'}
-      fontSize={['sm','md']}
-      fontWeight={'normal'}
-      maxWidth={'90%'}
-      >
-        {project.description}
-      </Text>
-      <Link href={project.source_code} isExternal
-      outlineColor={'transparent'}
-      _hover={{
-        color: 'blue.600',
-        transition: 'color .25s'
-      }}
-      >
-      <Flex mt={'4'} w='full' alignItems="center">
-        <Icon fontSize={'md'} as={GoMarkGithub} mr='3' />
-        <Text fontSize={'smaller'}>Github Repository</Text>
-      </Flex>
-      </Link>
-              </Box>
+              <ProjectCard key={project.id} project={project}/>
             ))
           }
           </Box>
@@ -153,6 +120,7 @@ export const getServerSideProps = async () => {
 
   const query = '*[_type == "project"]';
   const projects = await client.fetch(query);
+
 
   return {
     props: {
